94738ae00fdc6cf70d7a07791dda144bb4452f2c
salorod <salorod@localhost>
2023-04-29 12:25:19 -0500

    First commit 04/29/2023

---
+++ Salo_Selenium/src/test/java/com/google/java/firstTest.java
@@ -0,0 +1,25 @@
+package com.google.java;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class firstTest {
+  @Test
+  public void f() {
+	  	ChromeOptions chromeOpt = new ChromeOptions();
+		WebDriverManager.chromedriver().setup();
+		//chromeOpt.addArguments("user-data-dir=/Users/ricardoavalos/Library/Application Support/Google/Chrome/Default");
+		WebDriver driver = new ChromeDriver(chromeOpt);
+ 
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+ 
+		Assert.assertEquals(driver.getCurrentUrl(), "https://www.google.com/");
+
+  }
+}
